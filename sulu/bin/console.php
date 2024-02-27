cee1893af095061d479e8188080d79391e7184e6
Johannes Wachter <johannes@sulu.io>
2024-02-27 09:44:19 +0100

    init sulu and sylius applications

---
+++ sulu/bin/console.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+if (!\is_file(\dirname(__DIR__) . '/vendor/autoload_runtime.php')) {
+    throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+}
+
+require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+if (!isset($suluContext)) {
+    $suluContext = Kernel::CONTEXT_ADMIN;
+}
+
+return function (array $context) use ($suluContext) {
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG'], $suluContext);
+
+    return new Application($kernel);
+};
---
+++ sulu/src/Kernel.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use FOS\HttpCache\SymfonyCache\HttpCacheProvider;
+use Sulu\Bundle\HttpCacheBundle\Cache\SuluHttpCache;
+use Sulu\Component\HttpKernel\SuluKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class Kernel extends SuluKernel implements HttpCacheProvider
+{
+    private ?HttpKernelInterface $httpCache = null;
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        parent::configureContainer($container, $loader);
+    }
+
+    public function getHttpCache(): HttpKernelInterface
+    {
+        if (!$this->httpCache instanceof HttpKernelInterface) {
+            $this->httpCache = new SuluHttpCache($this);
+            // Activate the following for user based caching see also:
+            // https://foshttpcachebundle.readthedocs.io/en/latest/features/user-context.html
+            //
+            // $this->httpCache->addSubscriber(
+            //    new \FOS\HttpCache\SymfonyCache\UserContextListener([
+            //        'session_name_prefix' => 'SULUSESSID',
+            //    ])
+            // );
+        }
+
+        return $this->httpCache;
+    }
+}
